30bedf3f1cf94bf13507b1f12c39470a42ce78a6
Stofull <yusif209@gmail.com>
2024-07-30 04:05:10 +0400

    Nya

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import TitleSection from "./components/TitleSection";
 import CarouselSection from "./components/CarouselSection";
 import Footer from "./components/Footer";
 import GradientSeparator from "./components/GradientSeperator";
+
+
+import CursorFollow from "./CursorFollow";
+import "./styles/App.css";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <CursorFollow />
       <Navbar />
       <div className="flex-grow">
         <TitleSection />
         <GradientSeparator />
         <CarouselSection />
         <GradientSeparator />
-
       </div>
       <Footer />
     </div>
